902e2c4089bcd01fb3fcb08c14bd6d728d7d749c
Maxim <mga@csgo.com>
2024-09-23 03:29:59 +0500

    обновил зависимость apollo-server + внёс запуск серва

--- src/db.js
+++ src/db.js
@@ -1,13 +1,28 @@
 const {Pool} = require('pg');
+const {pubSub} = require('./subscriptions');
 
 const pool = new Pool({
     user: 'testDB',
-    host: 'database',
+    host: 'localhost',
     database: 'testDB',
     password: '123456',
     port: 5432,
 });
 
 const query = (text, params) => pool.query(text, params);
+pool.connect((err, client, done) => {
+    if (err) throw err;
+
+    client.on('notification', msg => {
+        const payload = JSON.parse(msg.payload);
+        pool.query('SELECT * FROM users WHERE id = $1', [payload.sender_id], (err, res) => {
+            if (err) throw err;
+            payload.sender = res.rows[0];
+            pubSub.publish('MESSAGE_SENT', {messageSent: payload});
+        });
+    });
+
+    client.query('LISTEN new_message');
+});
 
 module.exports = {query};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+const {ApolloServer} = require('apollo-server-express');
+const express = require('express');
+const {createServer} = require('http');
+const {execute, subscribe} = require('graphql');
+const {SubscriptionServer} = require('subscriptions-transport-ws');
+const {makeExecutableSchema} = require('@graphql-tools/schema');
 const jwt = require('jsonwebtoken');
-const typeDefs = require("./schema");
-const resolvers = require("./resolvers");
-const {ApolloServer} = require("apollo-server");
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 const SECRET_KEY = 'testing123123';
 
 const getUserFromToken = (token) => {
@@ -15,12 +20,48 @@ const getUserFromToken = (token) => {
     return null;
 };
 
-const server = new ApolloServer({
+const app = express();
+
+const schema = makeExecutableSchema({
     typeDefs,
     resolvers,
+});
+
+const server = new ApolloServer({
+    schema,
     context: ({req}) => {
         const token = req.headers.authorization || '';
         const user = getUserFromToken(token.replace('Bearer ', ''));
         return {user};
     },
+});
+
+server.start().then(() => {
+    server.applyMiddleware({app});
+
+    const httpServer = createServer(app);
+
+    new SubscriptionServer(
+        {
+            execute,
+            subscribe,
+            schema,
+            onConnect: (connectionParams) => {
+                if (connectionParams.authorization) {
+                    const user = getUserFromToken(connectionParams.authorization);
+                    return {user};
+                }
+                throw new Error('Авторизация требуется!');
+            },
+        },
+        {
+            server: httpServer,
+            path: server.graphqlPath,
+        }
+    );
+
+    httpServer.listen(4001, () => {
+        console.log(`🚀 Сервер запущен: http://localhost:4000${server.graphqlPath}`);
+        console.log(`🚀 Подписки готовы по адресу ws://localhost:4000${server.graphqlPath}`);
+    });
 });
